2b3f22692fda0ee38928d87801d4a31ec24e80b5
pentonium <76650037+pentonium@users.noreply.github.com>
2021-03-19 16:49:19 +0530

    update

--- contracts/PTMToken.sol
+++ contracts/PTMToken.sol
@@ -2,8 +2,6 @@ pragma solidity ^0.6.0;
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 contract PTMToken is ERC20 {
-    string public _name = "PTMToken";
-    string public _symbol = "PTM";
     uint256 public INITIAL_SUPPLY = 100000000;
 
     constructor() public ERC20("PTMToken", "PTM") {
